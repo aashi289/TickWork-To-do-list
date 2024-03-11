abe04562e32738fa7348d0e401f9dd0b560e1193
Aastha <aasthasinghddun289@gmail.com>
2024-03-11 15:45:02 +0530

    first commit

---
+++ myjQuery.js
@@ -0,0 +1,74 @@
+var list = document.querySelector('ul');
+list.addEventListener('click', function(ev) {
+  if (ev.target.tagName === 'LI') {
+    ev.target.classList.toggle('checked');
+  }
+}, false);
+
+
+$(document).ready(function () {
+
+  console.log('I am in a new file now');
+
+  //Your jquery code here
+  console.log("We are using jQuery");
+
+  $('.btn').click(function () {
+
+    $('.wiki').slideToggle(1000);
+  })
+
+
+  $('.addBtn').click(function () {
+    var li = document.createElement("li");
+    var inputValue = document.getElementById("myInput").value;
+    var t = document.createTextNode(inputValue);
+    li.appendChild(t);
+    if (inputValue === '') {
+      alert("You must write something!");
+    } else {
+      document.getElementById("myUL").appendChild(li);
+    }
+    document.getElementById("myInput").value = "";
+
+    var span = document.createElement("SPAN");
+    var txt = document.createTextNode("\u00D7");
+    span.className = "close";
+    span.appendChild(txt);
+    li.appendChild(span);
+
+    for (i = 0; i < close.length; i++) {
+      close[i].onclick = function () {
+        var div = this.parentElement;
+        div.style.display = "none";
+      }
+    }
+  })
+
+
+  var close = document.getElementsByClassName("close");
+var i;
+for (i = 0; i < close.length; i++) {
+  close[i].onclick = function() {
+    var div = this.parentElement;
+    div.style.display = "none";
+    
+  }
+}
+  
+
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
